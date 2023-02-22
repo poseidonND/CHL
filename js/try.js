f910fa450adedde5585bcafd90740955329c7b3b
Aman <atejaswi@nd.edu>
2023-02-22 15:30:07 -0500

    mhmngf

--- js/try.js
+++ js/try.js
@@ -3,7 +3,7 @@ var map = L.map("map").setView([52.8, 18], 4);
         // Base layers
         //  .. OpenStreetMap
         var osm = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-    maxZoom: 10,
+    maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
         'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     id: 'mapbox/streets-v11',
